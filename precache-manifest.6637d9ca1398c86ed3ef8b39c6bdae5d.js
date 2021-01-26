794a7930f218112c68d0b88c01afb5db95f1cb6f
nfrontero20 <nfrontero20@amherst.edu>
2021-01-26 13:35:13 -0500

    Updates

---
+++ precache-manifest.6637d9ca1398c86ed3ef8b39c6bdae5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303b2b6ef50451a80c3f7715e19ad7d1",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "739e3c9b8c4f77f32b27",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739e3c9b8c4f77f32b27",
+    "url": "/home/static/js/main.009ec700.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
